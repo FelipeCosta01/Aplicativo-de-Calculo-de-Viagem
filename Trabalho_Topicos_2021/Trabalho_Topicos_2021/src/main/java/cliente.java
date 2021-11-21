13fdac547e3510e1bed8354ef4f9f70b5c70cf7d
Felipe Costa <lipecatornado@gmail.com>
2021-11-21 17:59:01 -0300

    Envio

---
+++ Trabalho_Topicos_2021/Trabalho_Topicos_2021/src/main/java/cliente.java
@@ -0,0 +1,29 @@
+
+public class cliente {
+
+	private int codcli;
+	private String nomcli;
+	private String email_cli;
+	
+	public int getCodcli() {
+		return codcli;
+	}
+	public void setCodcli(int codcli) {
+		this.codcli = codcli;
+	}
+	public String getNomcli() {
+		return nomcli;
+	}
+	public void setNomcli(String nomcli) {
+		this.nomcli = nomcli;
+	}
+	public String getEmail_cli() {
+		return email_cli;
+	}
+	public void setEmail_cli(String email_cli) {
+		this.email_cli = email_cli;
+	}
+	
+	
+	
+}
---
+++ Trabalho_Topicos_2021/Trabalho_Topicos_2021/src/main/java/vendedor.java
@@ -0,0 +1,35 @@
+
+public class vendedor {
+
+	private int codven;
+	private String nomven;
+	private String email_ven;
+	private float percent_comissao;
+	public int getCodven() {
+		return codven;
+	}
+	public void setCodven(int codven) {
+		this.codven = codven;
+	}
+	public String getNomven() {
+		return nomven;
+	}
+	public void setNomven(String nomven) {
+		this.nomven = nomven;
+	}
+	public String getEmail_ven() {
+		return email_ven;
+	}
+	public void setEmail_ven(String email_ven) {
+		this.email_ven = email_ven;
+	}
+	public float getPercent_comissao() {
+		return percent_comissao;
+	}
+	public void setPercent_comissao(float percent_comissao) {
+		this.percent_comissao = percent_comissao;
+	}
+	
+	
+	
+}
